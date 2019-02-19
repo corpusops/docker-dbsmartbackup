9e933cccb3bf5f2ad8a4a49a6f7e65587d2c0ed2
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2019-02-19 21:18:25 +0100

    Finish dockerization

---
+++ rootfs/bin/dbs-entry.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env sh
+set -ex
+SDEBUG=${SDEBUG-}
+log() { echo "$@">&2; }
+vv() { log "$@";"$@"; }
+export BACKUP_TYPE=${BACKUP_TYPE-}
+if [ "x${BACKUP_TYPE}" = "x" ];then
+    log "no \$BACKUP_TYPE"
+    exit 1
+fi
+export PORT="${PORT-}"
+export DBS_SUPERVISORD_CONFIGS="${DBS_SUPERVISORD_CONFIGS:-"/etc/supervisor.d/rsyslog /etc/supervisor.d/cron"}"
+export SUPERVISORD_CONFIGS="${DBS_SUPERVISORD_CONFIGS}"
+export DBS_PERIODICITY="${DBS_USER:-"0 3 * * *"}"
+export DBS_USER="${DBS_USER:-"root"}"
+export DBS_COMMAND="${DBS_COMMAND:-"/usr/local/bin/run_dbsmartbackup.sh --quiet --no-colors"}"
+export DBS_AUTOCONF=${DBS_AUTOCONF:-1}
+export DBS_CRONTAB="${DBS_CRONTAB:-"/conf/templates/crontab.frep"}"
+export DBS_CONF_DEST="${DBS_CONF_DEST:-"/conf/dbs.conf"}"
+export DBS_CONF="${DBS_CONF:-"/conf/templates/conf.frep"}"
+export IS_DCRON="${IS_DCRON-}"
+if ( echo $BACKUP_TYPE | grep -iq post );then
+    export PASSWORD="${PASSWORD:-${POSTGRES_PASSWORD:-${PGPASSWORD:-${POSTGRES_PASSWORD-}}}}"
+    export DBUSER="${DBUSER:-${POSTGRES_USER-}}"
+elif ( echo $BACKUP_TYPE | grep -iq mysql );then
+    export PASSWORD="${PASSWORD:-${MYSQL_PASSWORD}}"
+    export DBUSER="${DBUSER:-${MYSQL_USER-}}"
+    if [  "x${NO_AUTO_PORT-}" = "x" ];then
+        if [ "x$PORT" != "x"  ];then export MYSQL_PORT="$PORT";fi
+    fi
+else
+    export PASSWORD="${PASSWORD-}"
+    export DBUSER="${DBUSER-}"
+fi
+if [ "${IS_DCRON}" = "x" ] && [ ! -e /etc/cron.d ];then
+    export IS_DCRON="0"
+    for i in /etc/crontabs /var/spool/crontabs;do
+        if [ -e "$i" ];then
+            export IS_DCRON="1"
+            export CRONTABS_SPOOL="${CRONTABS_SPOOL:-$i}"
+            break
+        fi
+    done
+fi
+if [ "x${IS_DCRON}" = "1" ];then
+    export CRONTABS_SPOOL="${CRONTABS_SPOOL:-"/etc/crontabs"}"
+    export DBS_CRONTAB_DEST="${DBS_CRONTAB_DEST:-"$CRONTABS_SPOOL/root"}"
+else
+    export CRONTABS_SPOOL="${CRONTABS_SPOOL:-"/etc/cron.d"}"
+    export DBS_CRONTAB_DEST="${DBS_CRONTAB_DEST:-"$CRONTABS_SPOOL/dbs"}"
+fi
+if [  "x${NO_AUTO_PASSWORD-}" = "x" ];then
+    if [ "x$PASSWORD" != "x"  ];then export PGPASSWORD="$PASSWORD";fi
+    if [ "x$DBUSER" != "x" ];then export PGUSER="$DBUSER";fi
+fi
+export DBS_CONF_DESTS="${DBS_CONF_DESTS:-"$DBS_CONF_DEST"}"
+if [ "x$SDEBUG" != "x" ];then set -x;fi
+if [ "x$DBS_AUTOCONF" = "x1" ];then
+    vv frep --overwrite $DBS_CRONTAB:$DBS_CRONTAB_DEST
+    vv frep --overwrite $DBS_CONF:$DBS_CONF_DEST
+fi
+if [ "x$@" = "x" ];then
+    set -- /bin/supervisord.sh
+fi
+log "Running $@"
+exec ${@}
+# vim:set et sts=4 ts=4 tw=80:
