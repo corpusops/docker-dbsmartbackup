976afa3af4efaf285d1b8e4c45825c7d01d51d3a
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2023-03-21 22:47:09 +0200

    fix for bindir

--- main.sh
+++ main.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 shopt -s extglob
+export DOCKER_BUILDKIT=${DOCKER_BUILDKIT-1}
+export COMPOSE_DOCKER_CLI_BUILD=${COMPOSE_DOCKER_CLI_BUILD-1}
+export BUILDKIT_PROGRESS=${BUILDKIT_PROGRESS-plain}
 ## refresh from corpsusops.bootstrap/hacking/shell_glue (copy paste until last function)
 readlinkf() {
     if ( uname | egrep -iq "darwin|bsd" );then
