4efe9ec66e9360e0143ed0217910be05a6d40ef2
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2023-04-19 22:47:09 +0200

    improve rsyslog entrypoint

--- rootfs/bin/rsyslog.sh
+++ rootfs/bin/rsyslog.sh
@@ -13,7 +13,7 @@ export RSYSLOG_SPLITTED_CONFIGS=${RSYSLOG_SPLITTED_CONFIGS-}
 SDEBUG=${SDEBUG-}
 if [ "x$SDEBUG" != "x" ];then set -x;fi
 if [ -e "$RSYSLOG_INJECT_DIR" ];then
-    cp -rfv "$RSYSLOG_INJECT_DIR" /etc
+    cp -rfv "$RSYSLOG_INJECT_DIR"/. /etc/
 fi
 if [ "x${NO_DEFAULT_RSYSLOG_CONF}" = "x" ] && [ -e /etc/rsyslog.conf.frep ];then
     frep --overwrite /etc/rsyslog.conf.frep:/etc/rsyslog.conf
@@ -21,6 +21,7 @@ fi
 if [ "x${NO_DEFAULT_LOGROTATE_CONF}" = "x" ] && [ -e $LOGROTATE_CONF_DIR ];then
     for i in $(ls $LOGROTATE_CONF_DIR/*.conf.frep 2>/dev/null || true);do
         frep "$i:$LOGROTATE_CONF_DIR/$(basename $i .frep)" --overwrite
+        rm -fv "$i"
     done
 fi
 if [ "x${NO_DEFAULT_RSYSLOGD_CONF}" = "x" ] && [ -e $RSYSLOG_CONF_DIR ];then
